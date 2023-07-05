b51bcf1c7224953f28c1cea19ce636909bda6d85
“Elinnanase” <130899797+Elinnanase@users.noreply.github.com>
2023-07-05 07:38:55 +0000

    Initial Commit1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @user = current_user
+    if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book)
+    else
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) # 削除するPostImageレコードを取得
+    book.destroy # レコードを削除
+    redirect_to books_path # PostImageの一覧ページへのパス
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated the book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: 'about'
+  devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :index, :show, :edit, :update]
+
+end
